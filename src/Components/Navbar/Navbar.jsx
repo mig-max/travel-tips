f4b0639daf974d373b7c9e40a91a96e869d8f1e5
Max Wellings <maxmechlin@gmail.com>
2024-02-16 17:52:25 +0100

    navbar and navbaritems implemented with navlink and tailwind installation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
 import { NavLink } from 'react-router-dom'
+<<<<<<< HEAD
 
+=======
+//import {IconName} from 'react-icons'
+import { HiHome,
+    HiMagnifyingGlass,
+    HiStar,
+    HiPlayCircle,
+    HiTv } from "react-icons/hi2";
+import { HiPlus,  HiDotsVertical } from 'react-icons/hi';
+>>>>>>> 914eec5 (navbar and navbaritems implemented with navlink and tailwind installation)
 
 
 
 function Navbar() {
+<<<<<<< HEAD
+=======
+
+    const menu=[
+        {
+          name: 'Home',
+          icon: HiHome
+          path: '/'
+        },
+        {
+          name: 'Search',
+          icon: HiMagnifyingGlass
+          path: '/search'
+        },
+        {
+          name: 'Favorites',
+          icon: HiStar
+          path: '/favorites'
+        }
+    ]
+>>>>>>> 914eec5 (navbar and navbaritems implemented with navlink and tailwind installation)
     
 
   return (
-    <div>
-    <nav className="navbar">
-      <h1>TipTravel</h1>
-      <img src={logo} alt="logo" className="w-[80px] md:w-[115px] object-cover"/>
-      {menu.map((item, index) => (
-        <NavLink to={item.name} key={index} className="navbar-nav navbar">{item.name}</NavLink>
+    <div className='flex items-center gap-8 bg-blue-500 bg-opacity-50'>
+      <img src={logo} alt="logo" className='w-[80px] md:w-[115px] object-cover'/>
+      {menu.map((item) => (
+        <NavLink to={item.path} exact key={item.name}>
+        <NavbarItem name={item.name} Icon={item.icon} />
+        </NavLink>
       ))}
-    </nav>
   </div>
   )
 }
--- src/Components/Navbar/NavbarItem.jsx
+++ src/Components/Navbar/NavbarItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function NavbarItem() {
+function NavbarItem({name, Icon}) {
   return (
-    <div className=''></div>
+    <div className='flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8'>
+        <Icon/>
+        <h2>{name}</h2>
+    </div>
   )
 }
 
