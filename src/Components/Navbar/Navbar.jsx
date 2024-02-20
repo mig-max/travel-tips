2d42d39d608e06932901e6e72f4e88d632c71217
Max Wellings <maxmechlin@gmail.com>
2024-02-20 12:39:22 +0100

    mobile size drop-down menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import logo from '../../assets/logo2.png'
 import { NavLink } from 'react-router-dom'
 import { HiHome, HiMagnifyingGlass, HiStar } from "react-icons/hi2";
+import { HiDotsVertical } from "react-icons/hi"
 import { IoIosAddCircle, IoMdContact } from "react-icons/io";
 import { RiTeamFill } from "react-icons/ri";
-
+import { useState } from 'react';
 
 function Navbar() {
+
+  const [toggle, setToggle] = useState(false)
     
   const menu=[
     {
@@ -43,11 +46,30 @@ function Navbar() {
   return (
     <div className='flex items-center gap-8 bg-blue-500 bg-opacity-50'>
       <img src={logo} alt="logo" className='w-[80px] md:w-[115px] object-cover'/>
+      <div className='hidden md:flex gap-8'>
       {menu.map((item) => (
         <NavLink to={item.path} exact key={item.name}>
         <NavbarItem name={item.name} Icon={item.icon} />
         </NavLink>
       ))}
+  </div>
+    <div className='flex md:hidden gap-8' onClick={()=> setToggle(!toggle)}>
+      {menu.map((item, index) => index<3&&(
+        <NavLink to={item.path} exact key={item.name}>
+        <NavbarItem name={''} Icon={item.icon} />
+        </NavLink>
+      ))}
+      <div className='md:hidden'>
+        <NavbarItem name={''} Icon={HiDotsVertical} />
+      {toggle? <div className='absolute mt-3'>
+      {menu.map((item, index) => index>3&&(
+        <NavLink to={item.path} exact key={item.name}>
+        <NavbarItem name={item.name} Icon={item.icon} />
+        </NavLink>
+      ))}
+      </div> : null}
+      </div>
+  </div>
   </div>
   )
 }
--- src/Components/Navbar/NavbarItem.jsx
+++ src/Components/Navbar/NavbarItem.jsx
@@ -1,9 +1,9 @@
 
 function NavbarItem({name, Icon}) {  
   return (
-    <div className='flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8'>
+    <div className='flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8 mb-3'>
         <Icon/>
-        <h2>{name}</h2>
+        <h2 className="hidden md:block ">{name}</h2>
     </div>
   )
 }
